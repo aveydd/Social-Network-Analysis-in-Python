e8d7deed4c4f0e83484f3ecfb9348c7453cdad04
Vaibhav <6288432+aveydd@users.noreply.github.com>
2018-10-02 22:21:11 +0530

    Create asym_net.py

---
+++ asym_net.py
@@ -0,0 +1,8 @@
+G_asymmetric = nx.DiGraph()
+G_asymmetric.add_edge('A','B')
+G_asymmetric.add_edge('A','D')
+G_asymmetric.add_edge('C','A')
+G_asymmetric.add_edge('D','E')
+
+nx.spring_layout(G_asymmetric)
+nx.draw_networkx(G_asymmetric)
