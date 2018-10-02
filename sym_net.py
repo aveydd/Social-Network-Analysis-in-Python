348750e73966ef8db18cac81c147511e49aedc63
Vaibhav <6288432+aveydd@users.noreply.github.com>
2018-10-02 22:19:50 +0530

    Create sym_net.py

---
+++ sym_net.py
@@ -0,0 +1,10 @@
+import networkx as nx
+G_symmetric = nx.Graph()
+G_symmetric.add_edge('Amitabh Bachchan','Abhishek Bachchan')
+G_symmetric.add_edge('Amitabh Bachchan','Aamir Khan')
+G_symmetric.add_edge('Amitabh Bachchan','Akshay Kumar')
+G_symmetric.add_edge('Amitabh Bachchan','Dev Anand')
+G_symmetric.add_edge('Abhishek Bachchan','Aamir Khan')
+G_symmetric.add_edge('Abhishek Bachchan','Akshay Kumar')
+G_symmetric.add_edge('Abhishek Bachchan','Dev Anand')
+G_symmetric.add_edge('Dev Anand','Aamir Khan')
